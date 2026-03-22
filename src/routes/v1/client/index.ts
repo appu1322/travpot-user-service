567d1f8e11ffd1089165c6cc953f0864aa829ed7
Apoorv Saxena <pannu5272@gmail.com>
2026-03-22 20:53:14 +0530

    [Add]
    add authentication

--- src/routes/v1/client/index.ts
+++ src/routes/v1/client/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { authRouter } from './auth.routes';
 import { userRouter } from './user.routes';
+import { authenticate } from '../../../middlewares';
 
 const router = Router();
 
 router.use('/auth', authRouter);
-router.use('/user', userRouter);
+router.use('/user', authenticate, userRouter);
 
 export const clientRouter = router;
--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -3,9 +3,6 @@ import { clientRouter } from './client';
 
 const router = Router();
 
-// Public Route
-
-// Private Route
 router.use('/', clientRouter);
 
 export const v1Routers = router;
